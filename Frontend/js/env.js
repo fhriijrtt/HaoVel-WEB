394ae7d2ad59d521d84e1bb4a956c9b06683931f
1stHao <newbiegenshin0111@gmail.com>
2026-06-24 16:03:54 +0700

    Add files via upload

---
+++ Frontend/js/env.js
@@ -0,0 +1,6 @@
+'use strict';
+
+window.HAOVELS_ENV = {
+  SUPABASE_URL: 'https://dqgloiukgpmvetrramnc.supabase.co',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRxZ2xvaXVrZ3BtdmV0cnJhbW5jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIyMTU4NTEsImV4cCI6MjA5Nzc5MTg1MX0.4KPDO83u10mbBJOgtHIpqHAZWcuDpCQmDmP0cybmiFM',
+};
